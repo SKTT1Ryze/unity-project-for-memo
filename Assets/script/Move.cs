24513381aab62fcc9aaffdc8184c663f344aae15
SKTT1Ryze <1276675421@qq.com>
2019-10-02 00:41:41 +0800

    add changes of script

--- Assets/script/Move.cs
+++ Assets/script/Move.cs
@@ -5,5 +5,24 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     public float speed = 0.5f;
+    private bool Ismove = true;
+    private Vector3 pos;
 
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Ismove)
+        {
+            /*Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();*/
+            pos = Input.mousePosition;
+            Debug.Log(pos);
+
+
+        }
+    }
 }
