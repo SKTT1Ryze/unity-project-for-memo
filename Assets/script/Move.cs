4ed61c4d2cfcae986a13c977a9dbe88e8868e8ec
SKTT1Ryze <1276675421@qq.com>
2019-10-02 08:25:42 +0800

    update move script

--- Assets/script/Move.cs
+++ Assets/script/Move.cs
@@ -7,6 +7,7 @@ public class Move : MonoBehaviour
     public float speed = 0.5f;
     private bool Ismove = true;
     private Vector3 pos;
+    private float dist;
 
     void Start()
     {
@@ -19,10 +20,19 @@ public class Move : MonoBehaviour
         {
             /*Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit = new RaycastHit();*/
-            pos = Input.mousePosition;
-            Debug.Log(pos);
+            pos.x =Input .mousePosition .x -Screen .width *0.5f;
+            pos.y = Input.mousePosition.y - Screen.height*0.5f;
+            pos.z = transform.position.z;
+            Debug.Log("pos:"+pos);
+            Debug.Log("this:"+transform.position);
+            dist = Vector3.Distance(pos, transform.position);
+            Debug.Log("distance:"+dist);
+            Debug.Log("movevector:" + (pos - transform.position));
+            if(dist >10f)
+            {
+                this.transform.Translate((pos - transform.position) * Time.deltaTime * speed, Space.World);
 
-
+            }
         }
     }
 }
