8b37acad6fc39692a810efc66a97879158eeecb5
SKTT1Ryze <1276675421@qq.com>
2019-10-06 11:21:37 +0800

    搭建好了两个UI界面，吃完饭回来继续

---
+++ Assets/script/SelectModeSceneManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectModeSceneManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    public void ReturnStartScene()
+    {
+        Debug.Log("返回开始界面");
+    }
+    public void SelectModeOne()
+    {
+        Debug.Log("模式一");
+    }
+    public void SelectModeTwo()
+    {
+        Debug.Log("模式二");
+    }
+    public void SelectModeThree()
+    {
+        Debug.Log("模式三");
+    }
+    public void SelectModeFour()
+    {
+        Debug.Log("模式四");
+    }
+}
---
+++ Assets/script/startSceneManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startSceneManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    public void StartGame()
+    {
+        Debug.Log("开始游戏");
+    }
+    public void ExitGame()
+    {
+        Debug.Log("退出游戏");
+    }
+    public void EnterSetting()
+    {
+        Debug.Log("游戏设置");
+    }
+    public void EnterHelp()
+    {
+        Debug.Log("游戏帮助");
+    }
+}
